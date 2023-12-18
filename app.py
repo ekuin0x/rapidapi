b7e160eefb60dc37c35603be03d99f7659286c5c
ekuin0x <salhimouiz1@gmaiL.com>
2023-12-18 13:19:20 +0100

    news

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
-import random
-import requests
+import newspaper
+import json
 
 config = {
   "apiKey": "AIzaSyBFj4WtVAfarJFy0V6YwccqTNplYajofOo",
@@ -17,9 +17,28 @@ app = Flask(__name__)
 def index() : 
     return "<h1> HEY, STALKER! </h1>"
 
-@app.route("/test", methods = ["POST"])
+@app.route("/extract", methods = ["GET"])
 def test() : 
-    return "wassup"
+    url = request.args.get("url")
+    try : 
+        article = newspaper.Article(url=url, language='en')
+        article.download()
+        article.parse()
+
+        title = article.title 
+        text = article.text 
+        img = article.top_image
+        publish_date = datetime.now().strftime("%b-%d, %Y")
+
+        data = {
+            "title" : title,
+            "text" : text,
+            "image" : img,
+            "publish_date" : publish_date 
+        }
+return json.dumps(data)
+    except : 
+        return "Error occured with provided url"
         
 def index() : 
     return "<h1> HEY, STALKER! </h1>"
